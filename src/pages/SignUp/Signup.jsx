e2b4ed72aa88b7cb91c43a1b309b2deaec9b162d
DESKTOP-9LAGJ6P\jordan <jordan.wall@outlook.co.nz>
2022-10-27 13:49:58 +1300

    Front-End First Commit

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -5,19 +5,19 @@ import CardSmall from '../../components/Cards/CardSmall';
 const Signup = () => {
     return (
         <div>
-            <h1>Fill out our form</h1>
+            <h1 className="h3 red">Fill out our form</h1>
             <CardSmall>
                 <form>
-                    <label class={"text_label"}>First Name</label>
-                    <input type={"text"} placeholder={"Enter your first name"}/>
-                    <label>Last Name</label>
-                    <input type={"text"} placeholder={"Enter your surname"}/>
-                    <label>Email Address</label>
-                    <input type={"email"} placeholder={"Enter a valid email address"}/>
-                    <label>Date of Birth</label>
-                    <input type={"date"} placeholder={"Enter your date of birth"}/>
-                    <label>Password</label>
-                    <input type={"password"} placeholder={"Enter a secure password"}/>
+                    <div className="flex flex-column mb-20"><label className="label red fw-400">First Name</label>
+                    <input className="input" type="text" placeholder="Enter your first name"/></div>
+                    <div className="flex flex-column mb-20"><label className="label red fw-400">Last Name</label>
+                    <input className="input" type="text" placeholder="Enter your surname"/></div>
+                    <div className="flex flex-column mb-20"><label className="label red fw-400">Email Address</label>
+                    <input className="input" type="email" placeholder="Enter a valid email address"/></div>
+                    <div className="flex flex-column mb-20"><label className="label red fw-400">Date of Birth</label>
+                    <input className="input" type="date" placeholder="Enter your date of birth"/></div>
+                    <div className="flex flex-column mb-20"><label className="label red fw-400">Password</label>
+                    <input className="input" type="password" placeholder="Enter a secure password"/></div>
                     <button type="cancel">Cancel</button>
                     <button type="submit">Sign up!</button>
                 </form>
