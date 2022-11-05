64781523928878eebe3be546eb61a20d59163970
DESKTOP-9LAGJ6P\jordan <jordan.wall@outlook.co.nz>
2022-11-05 14:32:18 +1300

    Profile fixed

--- src/pages/Profile/EditPassword.jsx
+++ src/pages/Profile/EditPassword.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { useQuery } from '@tanstack/react-query';
+import axios from "axios";
 
 const EditPassword = () => {
+    const { isLoading, error, data } = useQuery(['viewUserInfoByUser'], () =>
+    axios.get('http://localhost:3001/users?id=3').then(res =>
+      res.data
+    )
+    )
+
+    if (isLoading) return 'Loading...'
+
+    if (error) return 'An error has occurred: ' + error.message
+
     return (
         <div className="ml-200 mr-200">
         <h1 className="h4 fw-400 text-center mb-50">Edit Password</h1>
-
+            <form>
+                <div className="flex flex-column mb-30">
+                    <label className="label red fw-400">Change your password:</label>
+                    <input className="input" type="password" placeholder="********"/>
+                </div>
+                <button className="primary-button full-width" type="submit">Confirm & Save</button>
+            </form>
     </div>
     )
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,29 +3,19 @@ import profile_bg from '../../assets/images/profile_bg.png';
 import userImage from '../../assets/images/userImage.png';
 import profile_selected from '../../assets/images/profile_selected.png';
 import PersonalInfo from "./PersonalInfo";
-import LoanHistory from './LoanHistory';
 import EditPassword from './EditPassword';
 
 const Profile = () => {
     const [showPersonalInfo, setShowPersonalInfo] = useState(true);
-    const [showLoanHistory, setShowLoanHistory] = useState(false);
     const [showEditPass, setShowEditPass] = useState(false);
 
     const showPersonalInfoHandler = () => {
         setShowPersonalInfo(true);
-        setShowLoanHistory(false);
-        setShowEditPass(false);
-    }
-
-    const showHistoryHandler = () => {
-        setShowPersonalInfo(false);
-        setShowLoanHistory(true);
         setShowEditPass(false);
     }
 
     const showEditPassHandler = () => {
         setShowPersonalInfo(false);
-        setShowLoanHistory(false);
         setShowEditPass(true);
     }
 
@@ -38,14 +28,11 @@ const Profile = () => {
                     <div className="profile-parent">
                         <img className="profile-bg" src={profile_bg}/>   
                         <img className="profile-img" src={userImage}/>
-                        <img className="profile-nav1_bg" src={profile_selected}/>
-                        <button type="cancel" className="profile_selected profile-nav1" onClick={showPersonalInfoHandler}>Personal Info</button>
-                        <button type="cancel" className="profile-button profile-nav2" onClick={showHistoryHandler}>Loan History</button>
-                        <button type="cancel" className="profile-button profile-nav3" onClick={showEditPassHandler}>Edit Password</button>
+                        <button type="cancel" className="profile-button profile-nav1" onClick={showPersonalInfoHandler}>Personal Info</button>
+                        <button type="cancel" className="profile-button profile-nav2" onClick={showEditPassHandler}>Edit Password</button>
                     </div>
                 </div>
                 {showPersonalInfo && <PersonalInfo/>}
-                {showLoanHistory && <LoanHistory/>}
                 {showEditPass && <EditPassword/>}
             </div>
         </div>
